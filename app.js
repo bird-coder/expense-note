6a9b671caa97d526d6d672816b7f448e05c49b7a
bird-coder <1037297660@qq.com>
2019-01-15 17:38:05 +0800

    版本跟新test

--- app.js
+++ app.js
@@ -1,9 +1,76 @@
 //app.js
 App({
-  onLaunch: function () {
+  onLaunch: function (res) {
+    let that = this
+    if (typeof wx.getUpdateManager === 'function'){
+      const updateManager = wx.getUpdateManager()
+      updateManager.onCheckForUpdate(function(res) {
+        console.log(res)
+        if (res.hasUpdate){
+          wx.showModal({
+            title: '更新提示',
+            content: '有新版本正在下载中！',
+          })
+        }else{
+          for (let i in res.query){
+            if (i === 'from'){
+              that.globalData.adFrom = res.query[i]
+            }
+          }
+        }
+      })
+      updateManager.onUpdateReady(function () {
+        wx.showModal({
+          title: '更新提示',
+          content: '新版本已经准备好，是否重启应用？',
+          success: function (res) {
+            if (res.confirm) {
+              // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+              updateManager.applyUpdate()
+              for (let i in res.query) {
+                if (i === "from") {
+                  that.globalData.adFrom = res.query[i];
+                }
+              }
+            }
+          }
+        })
+      })
+      updateManager.onUpdateFailed(function () {
+        // 新的版本下载失败
+        wx.showModal({
+          title: '更新提示',
+          content: '新版本下载失败，请删除图标重新搜索',
+          showCancel: false,
+          success: function (res) {
+            if (res.confirm) {
+              // 应用更新失败退出游戏
+              wx.navigateBack({
+                delta: 0
+              })
+            }
+          }
+        })
+      })
+    } else {
+      wx.showModal({
+        title: '更新提示',
+        content: '为了正常运行，建议您先升级至微信最高版本！',
+        showCancel: false,
+        success: function (res) {
+          if (res.confirm) {
+            // 微信版本过低退出游戏
+            wx.navigateBack({
+              delta: 0
+            })
+          }
+        }
+      })
+    }
     // 登录
     wx.login({
       success: res => {
+        console.log(res)
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       }
     })
@@ -14,6 +81,7 @@ App({
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           wx.getUserInfo({
             success: res => {
+              console.log(res)
               // 可以将 res 发送给后台解码出 unionId
               this.globalData.userInfo = res.userInfo
 
@@ -43,6 +111,7 @@ App({
   },
   globalData: {
     userInfo: null,
+    adFrom: "",
     env: 'dev'
   }
 })
